10956ff6d6260f9b6cee78dcd0dd416c37007b24
tobiisaravia <tttobis@gmail.com>
2022-08-19 09:57:59 -0300

    add cursor-color folder

--- login-form/src/App.js
+++ login-form/src/App.js
@@ -1,8 +1,9 @@
 import { Login } from './components/Login/login'
+import Register from './components/Register/register'
 function App() {
   return (
     <div className="App">
-        <Login/>
+        <Register/>
     </div>
   );
 }
--- login-form/src/components/Register/register.js
+++ login-form/src/components/Register/register.js
@@ -1,9 +1,14 @@
 import React from 'react'
-
+import './register.css'
 const Register = () => {
     return (
-        <div>
-            <h1>Cooming Soon!</h1>
+        <div className="container">
+            <div className="left-register">
+                <h1>there is the left side</h1>
+            </div>
+            <div className="right-register">
+                <h1>Cooming Soon!</h1>
+            </div>
         </div>
     )
 }

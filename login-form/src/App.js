e5f1785e937070a39be3fb4fe9717a9a14df7aac
tobiisaravia <tttobis@gmail.com>
2022-08-30 18:52:46 -0300

    css changes

--- login-form/src/App.js
+++ login-form/src/App.js
@@ -1,9 +1,9 @@
 import { Login } from './components/Login/login'
-import Register from './components/Register/register'
+//import Register from './components/Register/register'
 function App() {
   return (
     <div className="App">
-        <Register/>
+        <Login/>
     </div>
   );
 }
